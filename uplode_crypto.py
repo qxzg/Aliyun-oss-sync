6fd1e890693f31a3a6ce5529fcc1d09acc5881f6
qxzg <out@qxzg.xyz>
2021-01-30 22:45:53 +0800

    封装Verify_Remote_File_Integrity和Uplode_File_Encrypted函数。实现大文件断点续传

--- uplode_crypto.py
+++ uplode_crypto.py
@@ -4,40 +4,51 @@
 import os
 import sqlite3
 import oss2
+import hashlib
 from oss2.crypto import AliKMSProvider
-
+import hashlib
 import config
-from scan_files import Get_File_sha256
-
-MultipartUpload_Min_Filesize = (1024 * 1024 * 1024) * 13 # 当文件大小大于该值时使用断点续传（GB）。不得大于5GB
-assert MultipartUpload_Min_Filesize <= 1024 * 1024 * 1024 * 5, "断点续传启用大小必须小于5GB"
-
+from scan_files import Calculate_File_sha256
 
 auth = oss2.Auth(config.AccessKeyId, config.AccessKeySecret)
 kms_provider = AliKMSProvider(config.AccessKeyId, config.AccessKeySecret, config.KMSRegion, config.CMKID)
 bucket = oss2.CryptoBucket(auth, config.OssEndpoint, config.bucket_name, crypto_provider=kms_provider)
 
-key = 'motto.zip'
-content = open(key, "rb")
-filename = 'download.zip'
+uplode_file = "oss-sync.zip"
+download_filename = 'download.zip'
 
-org_file_sha256 = Get_File_sha256(key)
-# 上传文件。
-bucket.put_object(
-    key, content.read(),
-    headers={
-        "content-length": str(os.path.getsize(key)),
-        "x-oss-server-side-encryption": "KMS",
-        "x-oss-storage-class": "IA",
-        "x-oss-meta-sha256": org_file_sha256
-    }
-)
 
-# 下载OSS文件到本地内存。
-result = bucket.get_object(key)
-# 下载OSS文件到本地文件。
-result = bucket.get_object_to_file(key, filename)
-if Get_File_sha256(filename) == org_file_sha256:
-    print("校验通过")
-else:
-    print("数据不匹配")
+def Uplode_File_Encrypted(uplode_file):
+    org_file_sha256 = Calculate_File_sha256(uplode_file)  # TODO 从json获取文件sha256
+    oss2.resumable_upload(
+        bucket, uplode_file, uplode_file,
+        # store=oss2.ResumableStore(root='/tmp'),
+        multipart_threshold=1024*1024*50,
+        part_size=1024*1024*50,
+        num_threads=4,
+        headers={
+            "content-length": str(os.path.getsize(uplode_file)),
+            "x-oss-server-side-encryption": "KMS",
+            "x-oss-storage-class": "IA",
+            "x-oss-meta-sha256": org_file_sha256
+        }
+    )
+
+
+def Verify_Remote_File_Integrity(remote_file):
+    result = bucket.get_object(remote_file)
+    sha256 = hashlib.sha256()
+    for chunk in result:
+        sha256.update(chunk)
+    print(result.headers)
+    if sha256.hexdigest() == result.headers['x-oss-meta-sha256']:
+        print("校验通过")
+    else:
+        print("数据不匹配")
+
+
+if __name__ == "__main__":
+    pass
+    #Uplode_File_Encrypted(uplode_file)
+    # 下载OSS文件到本地文件。
+    #result = bucket.get_object_to_file(uplode_file, download_filename)
