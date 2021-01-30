724a998d3c0347b6240520dd53265dfcf35c3f6b
qxzg <out@qxzg.xyz>
2021-01-30 21:00:52 +0800

    增加Sha256和存储类型请求头

--- uplode_crypto.py
+++ uplode_crypto.py
@@ -7,42 +7,37 @@ import oss2
 from oss2.crypto import AliKMSProvider
 
 import config
+from scan_files import Get_File_sha256
 
 MultipartUpload_Min_Filesize = (1024 * 1024 * 1024) * 13 # 当文件大小大于该值时使用断点续传（GB）。不得大于5GB
 assert MultipartUpload_Min_Filesize <= 1024 * 1024 * 1024 * 5, "断点续传启用大小必须小于5GB"
-
-def get_content_length(data):
-    length = len(data.keys()) * 2 - 1
-    total = ''.join(list(data.keys()) + list(data.values()))
-    length += len(total)
-    return length
 
 
 auth = oss2.Auth(config.AccessKeyId, config.AccessKeySecret)
 kms_provider = AliKMSProvider(config.AccessKeyId, config.AccessKeySecret, config.KMSRegion, config.CMKID)
 bucket = oss2.CryptoBucket(auth, config.OssEndpoint, config.bucket_name, crypto_provider=kms_provider)
 
-
 key = 'motto.zip'
 content = open(key, "rb")
-filename = 'download.txt'
-print(type(b'a' * 1024 * 1024))
+filename = 'download.zip'
 
+org_file_sha256 = Get_File_sha256(key)
 # 上传文件。
-bucket.put_object(key, content.read(), headers={'content-length': str(os.path.getsize(key))})
+bucket.put_object(
+    key, content.read(),
+    headers={
+        "content-length": str(os.path.getsize(key)),
+        "x-oss-server-side-encryption": "KMS",
+        "x-oss-storage-class": "IA",
+        "x-oss-meta-sha256": org_file_sha256
+    }
+)
 
 # 下载OSS文件到本地内存。
 result = bucket.get_object(key)
 # 下载OSS文件到本地文件。
 result = bucket.get_object_to_file(key, filename)
-
-# 验证获取到的文件内容跟上传时的文件内容是否一致。
-content_got = b''
-for chunk in result:
-    content_got += chunk
-assert content_got == content
-
-
-# 验证获取到的文件内容跟上传时的文件内容是否一致。
-with open(filename, 'rb') as fileobj:
-    assert fileobj.read() == content
+if Get_File_sha256(filename) == org_file_sha256:
+    print("校验通过")
+else:
+    print("数据不匹配")
