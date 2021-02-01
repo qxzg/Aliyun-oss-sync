355a4f39318ec28f98470e30e5cec93fd3a4bde9
qxzg <out@qxzg.xyz>
2021-02-01 19:33:02 +0800

    更新config.py

--- config.example.py
+++ config.example.py
@@ -1,11 +1,15 @@
 AccessKeyId = ""
 AccessKeySecret = ""
-
-OssEndpoint = "oss-cn-hangzhou.aliyuncs.com"
+OssEndpoint = ""
+CMKID = ""
+KMSRegion = ""
 bucket_name = ""
 
-CMKID = ""
-KMSRegion = "cn-hongkong"
-
 local_bace_dir = "/mnt/"  # 本地工作目录（绝对路径, eg：/mnt/）
+remote_bace_dir = "nas-backup/"
 backup_dirs = ["personal/", "www/nextcloud/data/"]  # 备份目录（相对于local_bace_dir, eg:data/）
+
+temp_dir = "/tmp/oss-sync/"
+
+LogFile = "/root/oss-sync.log"
+LogFormat = "%(asctime)s - [%(levelname)s]: %(message)s"
