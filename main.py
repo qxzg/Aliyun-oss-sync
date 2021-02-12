8c1961b3f37cd3b9fb4bcb98bc9aeb7828c57b9a
qxzg <out@qxzg.xyz>
2021-02-12 15:58:28 +0800

    在计算sha256的同时上传文件

--- main.py
+++ main.py
@@ -44,26 +44,7 @@ if __name__ == "__main__":
                 totle_file_num += 1
                 totle_file_size += file_size
     logging.info("备份文件扫描完成\n备份文件总数：%d\n备份文件总大小：%.2f GB" % (totle_file_num, totle_file_size / (1024 * 1024 * 1024)))
-# 计算备份文件的sha256
-# else:
-    logging.info("开始计算sha256")
-    for path in local_files_sha256:  # TODO: 实现多线程计算sha256  doc: https://www.liaoxuefeng.com/wiki/1016959663602400/1017628290184064
-        sha256 = oss_sync_libs.Calculate_Local_File_sha256(path)
-        if sha256 != False:
-            local_files_sha256[path] = sha256
-        else:
-            del(local_files_sha256[path])
-    try:
-        with open(local_json_filename, 'w') as json_fileobj:
-            json.dump(local_files_sha256, json_fileobj)
-        logging.info("sha256计算完成，保存到" + local_json_filename)
-    except:
-        logging.exception("保存json文件时出错，无法保存至%s，尝试保存至%ssha256_local.json" % (local_json_filename, config.local_bace_dir))
-        with open(config.local_bace_dir + "sha256_local.json", 'w') as json_fileobj:
-            json.dump(local_files_sha256, json_fileobj)
 
-######################################################################
-# else:
     # 获取远程文件json
     oss.Download_Decrypted_File(remote_json_filename, "sha256.json")
     with open(remote_json_filename, 'r') as fobj:
@@ -72,40 +53,62 @@ if __name__ == "__main__":
     for file, sha256 in remote_files_sha256.items():
         sha256_to_remote_file[sha256] = file
 
+# 计算备份文件的sha256
+# else:
     copy_list = {}  # 需要复制的文件列表{目标文件: 源文件}
-    delete_list = []  # 需要删除的文件列表
     uplode_list = []  # 需要上传的文件列表
-    with open(local_json_filename, 'r') as fobj:  # Dev!!
-        local_files_sha256 = json.load(fobj)
-
-    for item, sha256 in local_files_sha256.items():
-        if item in remote_files_sha256:
-            if remote_files_sha256[item] == sha256:
+    logging.info("开始计算sha256")
+    for path in local_files_sha256:  # TODO: 实现多线程计算sha256  doc: https://www.liaoxuefeng.com/wiki/1016959663602400/1017628290184064
+        sha256 = oss_sync_libs.Calculate_Local_File_sha256(path)
+        if sha256 == False:
+            del(local_files_sha256[path])
+            continue
+        local_files_sha256[path] = sha256
+        if path in remote_files_sha256:
+            if remote_files_sha256[path] == sha256:
                 continue
             elif sha256 in sha256_to_remote_file:
-                copy_list[config.remote_bace_dir + item] = config.remote_bace_dir + sha256_to_remote_file[sha256]
+                copy_list[config.remote_bace_dir + path] = config.remote_bace_dir + sha256_to_remote_file[sha256]
             else:
-                uplode_list.append(item)
+                try:
+                    oss.Uplode_File_Encrypted(path, config.remote_bace_dir + path, storage_class=config.default_storage_class, file_sha256=sha256)
+                except FileNotFoundError:
+                    logging.warning("上传时无法找到文件%s" % path)
+                    del(local_files_sha256[path])
+                else:
+                    uplode_list.append(path)
         elif sha256 in sha256_to_remote_file:
-            copy_list[config.remote_bace_dir + item] = config.remote_bace_dir + sha256_to_remote_file[sha256]
+            copy_list[config.remote_bace_dir + path] = config.remote_bace_dir + sha256_to_remote_file[sha256]
         else:
-            uplode_list.append(item)
-    for item, sha256 in remote_files_sha256.items():
-        if not item in local_files_sha256:
-            delete_list.append(config.remote_bace_dir + item)
-    logging.info("开始同步远程文件")
+            try:
+                oss.Uplode_File_Encrypted(path, config.remote_bace_dir + path, storage_class=config.default_storage_class, file_sha256=sha256)
+            except FileNotFoundError:
+                logging.warning("上传时无法找到文件%s" % path)
+                del(local_files_sha256[path])
+            else:
+                uplode_list.append(path)
+
     if len(copy_list) != 0:
         oss.Copy_remote_files(copy_list)
+    delete_list = []  # 需要删除的文件列表
+    for path, sha256 in remote_files_sha256.items():
+        if not path in local_files_sha256:
+            delete_list.append(config.remote_bace_dir + path)
     if len(delete_list) != 0:
         oss.Delete_Remote_files(delete_list)
-    if len(uplode_list) != 0:
-        for file in uplode_list:
-            try:
-                oss.Uplode_File_Encrypted(file, config.remote_bace_dir + file, storage_class=config.default_storage_class, file_sha256=local_files_sha256[file])
-            except FileNotFoundError:
-                logging.warning("上传时无法找到文件%s" % file)
-                del(local_files_sha256[file])
-                uplode_list.remove(file)
+
+    try:
+        with open(local_json_filename, 'w') as json_fileobj:
+            json.dump(local_files_sha256, json_fileobj)
+        logging.info("sha256保存到" + local_json_filename)
+    except:
+        logging.exception("保存json文件时出错，无法保存至%s，尝试保存至%ssha256_local.json" % (local_json_filename, config.local_bace_dir))
+        with open(config.local_bace_dir + "sha256_local.json", 'w') as json_fileobj:
+            json.dump(local_files_sha256, json_fileobj)
+
+######################################################################
+# else:
+
     with open(local_json_filename, 'w') as fobj:
         json.dump(local_files_sha256, fobj)
     oss.Uplode_File_Encrypted(local_json_filename, 'sha256.json', storage_class='Standard')
