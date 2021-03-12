6d0060e910f9acb8865b7c336b46d7f3a558ee0b
qxzg <out@qxzg.xyz>
2021-03-12 20:26:22 +0800

    新增检查重建后的sha256与旧文件的区别

--- rebuild_sha256.py
+++ rebuild_sha256.py
@@ -12,6 +12,7 @@ import config
 import oss_sync_libs
 
 bucket = oss2.Bucket(oss2.Auth(config.OSSAccessKeyId, config.OSSAccessKeySecret), 'https://' + config.OssEndpoint, config.bucket_name)
+rebuild_file = 'sha256-rebuild.json'
 
 
 def Get_Remote_sha256(obj):
@@ -29,18 +30,31 @@ def Get_Remote_sha256(obj):
         return False
 
 
-sha256_to_files = {}
-err_files = []
-for obj in oss2.ObjectIteratorV2(bucket, prefix=config.remote_bace_dir):
-    obj = obj.key
-    if obj[-1] == '/':  # 判断obj为文件夹。
-        continue
-    sha256 = Get_Remote_sha256(obj)
-    if sha256 == False:
-        err_files.append(obj)
-    else:
-        sha256_to_files[obj[11:]] = sha256
-with open('sha256-rebuild.json', 'w') as fobj:
-    json.dump(sha256_to_files, fobj)
-print(err_files)
-print("无sha256的文件总数：" + str(len(err_files)))
+def check_diff():
+    with open(rebuild_file, 'r') as fobj:
+        nsha = json.load(fobj)
+    with open('sha256-old.json', 'r') as fobj:
+        osha = json.load(fobj)
+
+    dif = nsha.keys() - osha.keys()
+    with open('sha256.diff', 'w', encoding='utf-8') as fobj:
+        for i in iter(dif):
+            fobj.write(i + '\n')
+
+
+if __name__ == '__main__':
+    sha256_to_files = {}
+    err_files = []
+    for obj in oss2.ObjectIteratorV2(bucket, prefix=config.remote_bace_dir):
+        obj = obj.key
+        if obj[-1] == '/':  # 判断obj为文件夹。
+            continue
+        sha256 = Get_Remote_sha256(obj)
+        if sha256 == False:
+            err_files.append(obj)
+        else:
+            sha256_to_files[obj[11:]] = sha256
+    with open(rebuild_file, 'w') as fobj:
+        json.dump(sha256_to_files, fobj)
+    print(err_files)
+    print("无sha256的文件总数：" + str(len(err_files)))
